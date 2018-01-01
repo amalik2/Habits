8ff75b1a7b61b5ecb13283d2618fe900f2f9c046
amalik2 <619mwa@gmail.com>
2018-01-01 14:46:42 -0700

    refactoring habit view

--- src/views/edithabitview.js
+++ src/views/edithabitview.js
@@ -26,6 +26,13 @@ export default class EditHabitView extends HabitView {
 			this.state.daysOfWeekChecked[i] = true;
 	}
 	
+	/**
+	 * @return {Boolean} whether the selected habit title is valid and can be used to update the habit's details
+	 */
+	validateHabitTitle(){
+		return this.state.validateTitle(this.state.titleInputText) || this.state.habit.getTitle() === this.state.titleInputText;
+	}
+	
 	 /**
 	 * Handle the user clicking the confirm button
 	 * @param event - the event triggered by the button click
@@ -33,38 +40,12 @@ export default class EditHabitView extends HabitView {
 	 */
 	onConfirmClicked(event){
 		
-		// the habit that will be returned if cancel is false
 		let newHabit = null;
-		
-		// if the title is invalid, or a duplicate, prevent it from being used
-		if ((!this.state.validateTitle(this.state.titleInputText) && this.state.habit.getTitle() !== this.state.titleInputText)){
-			alert("The title was taken, or it was invalid");
-			return;
-		} else {
-		
-			let date = new Date(this.state.dateInputText);
-			if (!isValid(date)){
-				alert("The date was invalid");
-				return;
-			}
-			
-			// days of the week the habit will occur on
-			let days = [];
-			for (let i = 0; i < this.state.daysOfWeekChecked.length; ++i){
-				if (this.state.daysOfWeekChecked[i]){
-					days.push(i);
-				}
-			}
-			if (days.length === 0){
-				alert("No days of the week were selected");
-				return;
-			}
-			
-			// update the old habit's details
+		if (this.validateHabitDetails()){
 			this.state.habit.setTitle(this.state.titleInputText);
 			this.state.habit.setReason(this.state.reasonInputText);
-			this.state.habit.setStartDate(date);
-			this.state.habit.setDaysOfWeek(days);
+			this.state.habit.setStartDate(new Date(this.state.dateInputText));
+			this.state.habit.setDaysOfWeek(this.getSelectedDays());
 			newHabit = this.state.habit;
 		}
 		
